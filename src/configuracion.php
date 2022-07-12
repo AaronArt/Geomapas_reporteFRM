727fe679815429c92cece382b779321e5c303433
AaronArt <60679322+AaronArt@users.noreply.github.com>
2022-07-12 11:27:46 -0500

    Update configuracion.php
    Se configuro nueva BD

--- src/configuracion.php
+++ src/configuracion.php
@@ -1,10 +1,10 @@
 <?php
  #Archivo de configuracion de la base de datos
  
-        define("PG_DB"  , "proyectofinal");
-	define("PG_HOST", "localhost");
-	define("PG_USER", "user");
-	define("PG_PSWD", "user");
+        define("PG_DB"  , "d4jdnnep7hhvs5");
+	define("PG_HOST", "ec2-3-223-169-166.compute-1.amazonaws.com");
+	define("PG_USER", "hgtzrmzftbbktp");
+	define("PG_PSWD", "a3ec407f9b46c96c9eef970b1845026c9ab160640340d01c07b092075771ce6b");
 	define("PG_PORT", "5432");
 	
 	$dbcon = pg_connect("dbname=".PG_DB." host=".PG_HOST." user=".PG_USER ." password=".PG_PSWD." port=".PG_PORT."");
